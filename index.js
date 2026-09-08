74665dd9a156c3ac64bf66cad8e4903e53908fd5
legion-samobojcow <ewa.siemionek123@gmail.com>
2026-09-08 17:55:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ import {
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 const client = new Client({
